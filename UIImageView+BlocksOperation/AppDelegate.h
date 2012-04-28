f489adbb62519bbc6a7243854e98b75d3e8b0de0
t.hayashi <t.hayashi0077+github@gmail.com>
2012-04-29 00:18:36 +0900

    Initial Commit

---
+++ UIImageView+BlocksOperation/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UIImageView+BlocksOperation
+//
+//  Created by t_hayashi on 12/04/29.
+//  Copyright (c) 2012年 . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
